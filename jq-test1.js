06b978c721a6e99c91832a0562893a022c2e8c0e
vivoge <vivoge@163.com>
2016-09-15 13:08:19 +0800

    selector

--- jq-test1.js
+++ jq-test1.js
@@ -1,3 +1,11 @@
 $(function(){
-    $('#box').css('color','red');
+
+$('#box').css('color','blue');
+   // console.log($('li').length);
+   //alert($('img').size());
+   //$('li').eq(1).css('color','yellow');
+    //$('ul>li').css('color','pink')
+    console.log($('*'),$('*').length);
 });
+
+
