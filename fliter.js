37cffad84efbc0017b0694a854f1eba8f11de082
vivoge <vivoge@163.com>
2016-09-17 02:00:53 +0800

    selector

--- fliter.js
+++ fliter.js
@@ -1,11 +1,20 @@
 $(function(){
+    var i=0;
 $('.show').on('click',function(){
-  $('#box').show(100)
+
+        $('.test').first().show(100,function showNext(){
+            $(this).next().show(100,showNext)
+        })
+
 });
 
-    $('.hidden').on('click',function(){
-        $('#box').hide(100)
-    })
+
+
+
+    $('.hide').on('click',function(){
+        $('.test').hide(100)
+    });
+
 
 
 });
